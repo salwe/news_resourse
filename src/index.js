3f80dae5b98da18f2d7888af1b5cb464e846e63a
Salwe Breid <salwebreid@gmail.com>
2018-04-04 16:45:10 +0300

    scss added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
-import './css/App.css';
+import './css/App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
